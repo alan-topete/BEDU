c18b7715b209feed1fd96a22806fde7185fd06fd
Alan <69323721+alan-topete@users.noreply.github.com>
2020-10-29 09:53:44 -0600

    Update Reto_03.sql

--- Sesion_02/Reto_03.sql
+++ Sesion_02/Reto_03.sql
@@ -2,7 +2,7 @@ USE tienda;
 #¿Cuántos registros hay por cada uno de los puestos?
 SELECT nombre, COUNT(*) AS frecuencia FROM puesto GROUP BY nombre;
 #¿Cuánto dinero se paga en total por puesto?
-SELECT nombre, SUM(salario) totalPagado FROM puesto GROUP BY nombre;
+SELECT nombre, SUM(salario) AS totalPagado FROM puesto GROUP BY nombre;
 #¿Cuál es el número total de ventas por vendedor?
 SELECT id_empleado, COUNT(*) AS totalVentas FROM venta GROUP BY id_empleado;
 #¿Cuál es el número total de ventas por artículo?
