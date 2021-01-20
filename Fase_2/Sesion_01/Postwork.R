c5f5995a2acaee9c4bce5821f38e2c9b4075b440
Alan <69323721+alan-topete@users.noreply.github.com>
2021-01-19 18:49:01 -0600

    Update Postwork.R

--- Fase_2/Sesion_01/Postwork.R
+++ Fase_2/Sesion_01/Postwork.R
@@ -1,4 +1,11 @@
+#getting data
 data.la.liga <- read.csv("https://www.football-data.co.uk/mmz4281/1920/SP1.csv")
-round(table(data.la.liga$FTHG) / margin.table(table(data.la.liga$FTHG)), 2)
-round(table(data.la.liga$FTAG) / margin.table(table(data.la.liga$FTAG)), 2)
-round(table(data.la.liga$FTHG, data.la.liga$FTAG) / 380 * 100, 2)
+#home and away goals per match
+(data.la.liga$FTHG)
+(data.la.liga$FTAG)
+#documentation regarding table function
+?table
+#frequency tables for goals scored
+round(table(data.la.liga$FTHG) / margin.table(table(data.la.liga$FTHG)), 2) #relative freq home team goals
+round(table(data.la.liga$FTAG) / margin.table(table(data.la.liga$FTAG)), 2) #relative freq away team goals
+round(table(data.la.liga$FTHG, data.la.liga$FTAG) / 380 * 100, 2) #relative joint freq home and away teams goals
